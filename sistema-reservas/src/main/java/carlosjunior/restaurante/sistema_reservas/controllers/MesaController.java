ac9c1ce852883e94c4cc1127d27ac5faf6c8d10a
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2025-06-06 22:22:17 -0300

    feat: post mesas

--- sistema-reservas/src/main/java/carlosjunior/restaurante/sistema_reservas/controllers/MesaController.java
+++ sistema-reservas/src/main/java/carlosjunior/restaurante/sistema_reservas/controllers/MesaController.java
@@ -3,7 +3,10 @@ package carlosjunior.restaurante.sistema_reservas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import carlosjunior.restaurante.sistema_reservas.models.Mesa;
@@ -19,4 +22,12 @@ public class MesaController {
     public List<Mesa> getMesas(){
         return repository.findAll();
     }
+
+     @PostMapping("mesas")
+    public ResponseEntity<Mesa> createAnylis(@RequestBody Mesa mesa){
+        Mesa savedMesa = repository.save(mesa);
+        return ResponseEntity.ok(savedMesa);
+    }
+
+
 }
